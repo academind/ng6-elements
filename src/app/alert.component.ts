cc1f065e9c9836539f4d6b6d65c4aa08a6b2175f
Maximilian Schwarzmüller <company@academind.com>
2018-05-04 00:10:02 +0200

    finished angular element demo

---
+++ src/app/alert.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'app-alert',
+  template: '<div>This is an alert. {{ message }}</div>',
+  styles: [`
+    div {
+      border: 1px solid black;
+      background: salmon;
+      padding: 10px;
+      font-family: sans-serif;
+    }
+  `]
+})
+export class AlertComponent {
+  @Input() message: string;
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { AlertComponent } from './alert.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  content = null;
+
+  constructor(injector: Injector, domSanitizer: DomSanitizer) {
+    const AlertElement = createCustomElement(AlertComponent, {injector: injector});
+    customElements.define('my-alert', AlertElement);
+    setTimeout(() => {
+      this.content = domSanitizer.bypassSecurityTrustHtml("<my-alert message='Rendered dynamically'></my-alert>");
+    }, 1000);
+  }
+}
